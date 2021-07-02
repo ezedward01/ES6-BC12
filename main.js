978cb8acc5ebc430f01f58048937e0c621602e17
ezedward <trung.nguyen280901@hcmu.edu.vn>
2021-07-02 20:01:35 +0700

    add file 2/7/2021

---
+++ main.js
@@ -0,0 +1,44 @@
+/**
+ * Khai báo biến
+ * - Khai báo biến với let và const
+ * - So sánh giữa let, const, var
+ * - Nên dùng cái nào trong trường hợp nào
+ */
+
+//var
+// console.log(fullName); //work (undefined)
+// var fullName = 'Chu Ho Tay'
+// console.log(fullName);
+
+
+// function showErrowWithVar(){
+//     var isError = true;
+//     var error = '';
+
+//     if (isError){
+//         var error = 'Something went wrong!';
+//         console.log(error);
+//     }
+//     console.log(error);
+// }
+// showErrowWithVar();
+
+// let
+// let age = 18;
+// console.log(age);
+// let age = 20; toang//age has been declared
+
+// function showErrowWithVar(){
+//     let isError = true;
+//     let error = '';
+
+//     if (isError){
+//         let error = 'Something went wrong!';
+//         console.log(error);
+//     }
+//     console.log(error);
+// }
+
+// showErrowWithVar();
+
+//const
